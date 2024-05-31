76b0cca59b3f80b89506cfc3edec4c1102c12412
Ajopratama1 <a.ajopratama01@gmail.com>
2024-05-31 15:43:36 +0700

    Perbaiki penunjuk belakang yang melingkari antrian melingkar

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -17,14 +17,22 @@ class Queues {
         cin >> num;
         cout << endl;
 
-        if((FRONT == 0 && REAR == - 1) || (FRONT == REAR + 1)) {
+        if((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
             cout << "\nQueues overflow\n";
             return;
         }
 
-     if(FRONT == -1){
+        if(FRONT == -1){
             FRONT = 0;
             REAR = 0;
         }
-
-      
+        else {
+            if(REAR == max - 1) {
+                REAR = 0;
+            }
+            else{
+                REAR = REAR + 1;
+            }
+            
+        }
+     
